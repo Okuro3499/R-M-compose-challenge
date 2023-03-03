a092595b3d56bd63070b3fc48baf48df45124557
koleladan <isaiahodan@gmail.com>
2023-03-03 18:13:12 +0300

    bottom navigation done

--- app/src/main/java/com/compose/rmcomposechallenge/MainActivity.kt
+++ app/src/main/java/com/compose/rmcomposechallenge/MainActivity.kt
@@ -3,13 +3,32 @@ package com.compose.rmcomposechallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.compose.rmcomposechallenge.ui.theme.RMcomposechallengeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +36,171 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RMcomposechallengeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar ={ BottomNavigationBar(
+                        items = listOf(
+                            BottomNavItem(
+                                name = "Home",
+                                route = "home",
+                                icon = Icons.Default.Home
+                            ),
+                            BottomNavItem(
+                                name = "Files",
+                                route ="files",
+                                icon = Icons.Default.List,
+                                badgeCount = 56
+                            ),
+                            BottomNavItem(
+                                name = "Settings",
+                                route = "settings",
+                                icon = Icons.Default.Settings
+                            )
+                        ) ,
+                        navController = navController,
+                        onItemClick ={
+                            navController.navigate(it.route)
+                        }
+                    )
+                        
+                    }
                 ) {
-                    Greeting("Android")
+                    Navigation(navController = navController)
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Navigation(navController:NavHostController){
+    NavHost(navController = navController, startDestination = "home"){
+        composable("home"){
+            HomeScreen()
+
+        }
+        composable("files"){
+            MyFilesScreen()
+
+        }
+        composable("settings"){
+            SettingsScreen()
+
+        }
+    }
+
+
+}
+@Composable
+fun BottomNavigationBar(
+    //takes parameters to be displayed(items;icon, name and the route) which are wrapped in a data class
+    items:List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier =Modifier,
+    onItemClick:(BottomNavItem) -> Unit
+){
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.DarkGray,
+        elevation = 8.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.Green,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        if (item.badgeCount > 0){
+
+                            BadgedBox(
+                                badge= {
+
+                                    Text(text = item.badgeCount.toString(),)
+
+                                }
+                            )
+                            {
+                                Icon(
+                                    imageVector = item.icon,
+                                    contentDescription = item.name
+                                )
+
+                            }
+
+
+
+                        }
+                        else{
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.name
+                            )
+                        }
+
+                        if (selected){
+                            Text(
+                                text = item.name,
+                                textAlign = TextAlign.Center,
+                                fontSize = 12.sp
+
+                            )
+
+                        }
+                        
+                    }
+
+                }
+            )
+
+
+        }
+
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    RMcomposechallengeTheme {
-        Greeting("Android")
+fun HomeScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Home Screen")
+
     }
 }
+
+@Composable
+fun MyFilesScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Yellow),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Text(text = "Downloaded files" )
+
+
+    }
+}
+@Composable
+fun SettingsScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Transparent),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Settings Screen")
+
+    }
+}
+
