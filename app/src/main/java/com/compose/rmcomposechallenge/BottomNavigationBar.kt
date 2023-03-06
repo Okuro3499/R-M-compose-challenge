5db7f12beb1d9fe2189ba248fec81a46fe4ae2ce
koleladan <isaiahodan@gmail.com>
2023-03-06 20:59:48 +0300

    added sealed class for the screens

---
+++ app/src/main/java/com/compose/rmcomposechallenge/BottomNavigationBar.kt
@@ -0,0 +1,85 @@
+package com.compose.rmcomposechallenge
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomNavigationBar(
+    //takes parameters to be displayed(items;icon, name and the route) which are wrapped in a data class
+    items:List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick:(BottomNavItem) -> Unit
+){
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.DarkGray,
+        elevation = 8.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.Green,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        if (item.badgeCount > 0){
+
+                            BadgedBox(
+                                badge= {
+
+                                    Text(text = item.badgeCount.toString(),)
+
+                                }
+                            )
+                            {
+                                Icon(
+                                    imageVector = item.icon,
+                                    contentDescription = item.name
+                                )
+
+                            }
+
+
+
+                        }
+                        else{
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.name
+                            )
+                        }
+
+                        if (selected){
+                            Text(
+                                text = item.name,
+                                textAlign = TextAlign.Center,
+                                fontSize = 12.sp
+
+                            )
+
+                        }
+
+                    }
+
+                }
+            )
+
+
+        }
+
+    }
+
+}
--- app/src/main/java/com/compose/rmcomposechallenge/MainActivity.kt
+++ app/src/main/java/com/compose/rmcomposechallenge/MainActivity.kt
@@ -42,18 +42,18 @@ class MainActivity : ComponentActivity() {
                         items = listOf(
                             BottomNavItem(
                                 name = "Home",
-                                route = "home",
+                                route = Screens.HomeScreen.route,
                                 icon = Icons.Default.Home
                             ),
                             BottomNavItem(
                                 name = "Files",
-                                route ="files",
+                                route =Screens.MyFilesScreen.route,
                                 icon = Icons.Default.List,
                                 badgeCount = 56
                             ),
                             BottomNavItem(
                                 name = "Settings",
-                                route = "settings",
+                                route = Screens.SettingsScreen.route,
                                 icon = Icons.Default.Settings
                             )
                         ) ,
@@ -75,16 +75,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Navigation(navController:NavHostController){
-    NavHost(navController = navController, startDestination = "home"){
-        composable("home"){
+    NavHost(navController = navController, startDestination = Screens.HomeScreen.route){
+        composable(Screens.HomeScreen.route){
             HomeScreen()
 
         }
-        composable("files"){
+        composable(Screens.MyFilesScreen.route){
             MyFilesScreen()
 
         }
-        composable("settings"){
+        composable(Screens.SettingsScreen.route){
             SettingsScreen()
 
         }
@@ -92,77 +92,7 @@ fun Navigation(navController:NavHostController){
 
 
 }
-@Composable
-fun BottomNavigationBar(
-    //takes parameters to be displayed(items;icon, name and the route) which are wrapped in a data class
-    items:List<BottomNavItem>,
-    navController: NavController,
-    modifier: Modifier =Modifier,
-    onItemClick:(BottomNavItem) -> Unit
-){
-    val backStackEntry = navController.currentBackStackEntryAsState()
-    BottomNavigation(
-        modifier = modifier,
-        backgroundColor = Color.DarkGray,
-        elevation = 8.dp
-    ) {
-        items.forEach { item ->
-            val selected = item.route == backStackEntry.value?.destination?.route
-            BottomNavigationItem(
-                selected = selected,
-                onClick = { onItemClick(item) },
-                selectedContentColor = Color.Green,
-                unselectedContentColor = Color.Gray,
-                icon = {
-                    Column(horizontalAlignment = CenterHorizontally) {
-                        if (item.badgeCount > 0){
 
-                            BadgedBox(
-                                badge= {
-
-                                    Text(text = item.badgeCount.toString(),)
-
-                                }
-                            )
-                            {
-                                Icon(
-                                    imageVector = item.icon,
-                                    contentDescription = item.name
-                                )
-
-                            }
-
-
-
-                        }
-                        else{
-                            Icon(
-                                imageVector = item.icon,
-                                contentDescription = item.name
-                            )
-                        }
-
-                        if (selected){
-                            Text(
-                                text = item.name,
-                                textAlign = TextAlign.Center,
-                                fontSize = 12.sp
-
-                            )
-
-                        }
-                        
-                    }
-
-                }
-            )
-
-
-        }
-
-    }
-
-}
 
 @Composable
 fun HomeScreen(){
