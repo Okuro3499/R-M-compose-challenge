8fc1b56fb711c5fb3f27a4278506e6bad86ce967
fatahrez <fertahrez@gmail.com>
2023-03-08 21:08:55 +0300

    add styling for Home/Characters screen

--- app/src/main/java/com/compose/rmcomposechallenge/presentation/characters/HomeScreen.kt
+++ app/src/main/java/com/compose/rmcomposechallenge/presentation/characters/HomeScreen.kt
@@ -1,27 +1,122 @@
 package com.compose.rmcomposechallenge.presentation.characters
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.compose.rmcomposechallenge.domain.models.Result
+import com.compose.rmcomposechallenge.ui.theme.BlackBackground
 
 @Composable
 fun HomeScreen(){
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Cyan),
+            .background(BlackBackground),
         contentAlignment = Alignment.Center
     ) {
 
         val viewModel: CharactersViewModel = hiltViewModel()
 
         val state = viewModel.state.value
-        Text(text = state.characters.toString())
+
+        LazyColumn {
+            state.characters?.results?.let {
+                items(it.size) {i ->
+                    val character = it[i]
+                    RickAndMortyCharacter(character)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RickAndMortyCharacter(character: Result) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .clip(RoundedCornerShape(10)),
+        backgroundColor = Color.LightGray,
+        elevation = 4.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ){
+            AsyncImage(
+                model = character.image,
+                contentDescription = "Character Image",
+                modifier = Modifier.
+                        height(150.dp)
+            )
+
+            Column (
+                modifier = Modifier
+                    .fillMaxHeight()
+            ){
+                Text(
+                    text = character.name,
+                    style = MaterialTheme.typography.h6,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 8.dp, top = 4.dp)
+                )
+
+                val statusStringStyle = listOf( AnnotatedString.Range(
+                        SpanStyle(fontWeight = FontWeight.Bold),
+                        start = 0,
+                        end = 7
+                        )
+                    )
+                val statusString = "Status: ${character.status}"
+                Text(
+                    text = AnnotatedString(text = statusString, spanStyles = statusStringStyle),
+                    modifier = Modifier.padding(start = 8.dp, top = 4.dp)
+                )
+
+                val speciesStringStyle = listOf( AnnotatedString.Range(
+                    SpanStyle(fontWeight = FontWeight.Bold),
+                    start = 0,
+                    end = 8
+                )
+                )
+                val speciesString = "Species: ${character.species}"
+                Text(
+                    text = AnnotatedString(text = speciesString, spanStyles = speciesStringStyle),
+                    modifier = Modifier.padding(start = 8.dp, top = 4.dp)
+                )
+
+                val originStringStyle = listOf( AnnotatedString.Range(
+                    SpanStyle(fontWeight = FontWeight.Bold),
+                    start = 0,
+                    end = 7
+                )
+                )
+                val originString = "Origin: ${character.origin.name}"
+                Text(
+                    text = AnnotatedString(text = originString, spanStyles = originStringStyle),
+                    modifier = Modifier.padding(start = 8.dp, top = 4.dp, bottom = 4.dp)
+                )
+            }
+        }
     }
 }
