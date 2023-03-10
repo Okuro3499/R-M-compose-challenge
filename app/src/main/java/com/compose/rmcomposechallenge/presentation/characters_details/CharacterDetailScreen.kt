d617f1ecac1ec449e39695b440752f4a4eafc873
koleladan <isaiahodan@gmail.com>
2023-03-11 00:11:04 +0300

    main

--- app/src/main/java/com/compose/rmcomposechallenge/presentation/characters_details/CharacterDetailScreen.kt
+++ app/src/main/java/com/compose/rmcomposechallenge/presentation/characters_details/CharacterDetailScreen.kt
@@ -1,11 +1,21 @@
 package com.compose.rmcomposechallenge.presentation.characters_details
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.compose.rmcomposechallenge.R
+import com.compose.rmcomposechallenge.ui.theme.BlackBackground
 
 @Composable
 fun CharacterDetailScreen() {
@@ -16,5 +26,19 @@ fun CharacterDetailScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-    )
+            .background(BlackBackground),
+        contentAlignment = Alignment.Center
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 20.dp, top = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(stringResource(id = R.string.Dan),
+                style = MaterialTheme.typography.subtitle2
+            )
+
+        }
+    }
 }
