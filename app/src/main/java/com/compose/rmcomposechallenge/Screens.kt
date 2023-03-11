f7d84b93912aa30fb20df8eb95c8d69d9f4519e1
koleladan <isaiahodan@gmail.com>
2023-03-11 15:34:54 +0300

    charactersDetaiilsScreen added

--- app/src/main/java/com/compose/rmcomposechallenge/Screens.kt
+++ app/src/main/java/com/compose/rmcomposechallenge/Screens.kt
@@ -1,11 +1,11 @@
 package com.compose.rmcomposechallenge
 
 sealed class Screens(val route:String){
-    object HomeScreen: Screens("home_screen")
+    object SavedCharactersScreen: Screens("home_screen")
 
     object CharactersScreen: Screens("characters_screen")
 
-    object  SettingsScreen:Screens("settings_screen")
+
 
     object CharacterDetailScreen: Screens("character_detail_screen")
     object  AboutScreen:Screens("about_screen")
