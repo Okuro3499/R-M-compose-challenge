b05b319c1d55479a4cfe878347ac5f2cfa54ff7b
koleladan <isaiahodan@gmail.com>
2023-03-11 09:39:41 +0300

    main

--- app/src/main/java/com/compose/rmcomposechallenge/presentation/characters_details/CharacterDetailScreen.kt
+++ app/src/main/java/com/compose/rmcomposechallenge/presentation/characters_details/CharacterDetailScreen.kt
@@ -2,10 +2,7 @@ package com.compose.rmcomposechallenge.presentation.characters_details
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,11 +11,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import com.compose.rmcomposechallenge.R
 import com.compose.rmcomposechallenge.ui.theme.BlackBackground
 
 @Composable
-fun CharacterDetailScreen() {
+fun CharacterDetailScreen(character:Result) {
     val viewModel: CharacterDetailViewModel = hiltViewModel()
 
     val state = viewModel.state.value
@@ -29,16 +27,10 @@ fun CharacterDetailScreen() {
             .background(BlackBackground),
         contentAlignment = Alignment.Center
     ){
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(start = 20.dp, top = 20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(stringResource(id = R.string.Dan),
-                style = MaterialTheme.typography.subtitle2
-            )
 
-        }
     }
+
 }
+
+
+
